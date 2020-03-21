174893a2f92a21158a633a6b9dc7a3994bad9516
Francisco Possetto <franciscopossetto@hotmail.com>
2020-03-21 17:44:11 -0300

    Initial Commit

---
+++ Nina/ExternalApplication.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.UI;
+
+namespace Nina
+{
+    class ExternalApplication : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            /// <summary>
+            /// Create a new Tab on Ribbon Bar.
+            /// </summary>
+            const string RIBBON_TAB = "Revit API Extension";
+            Ribbon.CreateRibbonTab(application, RIBBON_TAB);
+
+            /// <summary>
+            /// Create a new Panel on Ribbon Tab.
+            /// </summary>
+
+            const string RIBBON_PANEL = "My Addins";
+            RibbonPanel ribbonPanel = Ribbon.CreateRibbonPanel(application, RIBBON_PANEL, RIBBON_TAB);
+
+            /// <summary>
+            /// Create new Buttons on Panel.
+            /// </summary>
+            /// 
+            const string PUSH_BUTTON_NAME = "Push Button";
+            const string PUSH_BUTTON_DESC = "This is a push button";
+
+            PushButtonData pushDataButton = Ribbon.CreatePushButtonData(PUSH_BUTTON_NAME, PUSH_BUTTON_DESC, "Nina.ExternalCommand");
+
+            PushButton pushButton = ribbonPanel.AddItem(pushDataButton) as PushButton;
+            System.Drawing.Bitmap ico = Properties.Resources.icon30;
+            System.Windows.Media.Imaging.BitmapSource icon30 = Ribbon.Icon(ico);
+            pushButton.LargeImage = icon30;
+
+            return Result.Succeeded;
+
+
+
+        }
+    }
+}
---
+++ Nina/ExternalCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace Nina
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+
+    public class ExternalCommand : IExternalCommand
+    {
+        /// <summary>
+        ///     External Command
+        /// </summary>
+        /// <param name ="commandData"></param>
+        /// <param name="message"></param>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                //Check Revit Version
+                if (!commandData.Application.Application.VersionName.Contains("2020"))
+                {
+                    using (TaskDialog taskDialog = new TaskDialog("Cannot Continue"))
+                    {
+                        taskDialog.TitleAutoPrefix = false;
+                        taskDialog.MainInstruction = "Incompatible Version of Revit";
+                        taskDialog.MainContent = "Main Content";
+                        taskDialog.Show();
+                    }
+                    return Result.Cancelled;
+                }
+
+                TaskDialog.Show("From Revit", "Hello World");
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
