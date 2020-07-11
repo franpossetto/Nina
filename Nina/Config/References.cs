592e7cfd38057e27b418a381e2acbe29044dd7fb
Francisco Possetto <franciscopossetto@hotmail.com>
2020-07-11 15:17:01 -0300

    some changes references file fluent bar and ui for wall type batch creation

---
+++ Nina/Config/References.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nina.Config
+{
+    AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+
+    static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+    {
+        if (/*some condition*/)
+            return Assembly.LoadFrom("DifferentDllFolder\\differentVersion.dll");
+        else
+            return Assembly.LoadFrom("");
+    }
+}
